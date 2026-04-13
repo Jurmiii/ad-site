0272b08dc4733423ad50d7d2f864cc13d9cc19ca
Jurmiii <kje89105@gmail.com>
2026-04-13 11:45:24 +0900

    초기화
    ㅇ

--- src/features/finance/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  initialFinanceState,
-  financeReducer,
-  selectBudgetBalances,
-  selectFinanceSummary,
-} from "@/features/finance/state";
-export { sampleFinanceState } from "@/features/finance/sample-data";
